aa41c90f7d477bc8938989ee490e628ab888c752
Mario Herrera <mario.herrera.flores@gmail.com>
2020-05-29 16:28:01 -0700

    Updated to profile component

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native';
 import {Divider, useTheme, Avatar, Title} from 'react-native-paper';
 import TextInput from '../components/TextInput';
@@ -28,51 +29,72 @@ export const Profile = () => {
         </TouchableOpacity>
         <Title style={styles.title}>Mario Herrera</Title>
         <Divider accessibilityStates />
+        <View style={styles.emailContainer}>
+          <MaterialCommunityIcons
+            style={styles.icon}
+            name={'phone'}
+            color={'#4FC3F7'}
+            size={35}
+          />
+          <TextInput
+            style={{
+              width: '50%',
+              backgroundColor,
+              justifyContent: 'center',
+            }}
+            mode={'flat'}
+            accessibilityStates
+            label={'Telefono'}
+            disabled={true}
+            returnKeyType={'done'}
+            value={'123-456-789'}
+          />
+        </View>
+        <View style={styles.emailContainer}>
+          <MaterialCommunityIcons
+            style={styles.icon}
+            name={'email'}
+            color={'#4FC3F7'}
+            size={35}
+          />
+          <TextInput
+            style={{
+              width: '50%',
+              backgroundColor,
+              justifyContent: 'center',
+            }}
+            mode={'flat'}
+            accessibilityStates
+            label={'Email'}
+            returnKeyType="done"
+            disabled={true}
+            value={'test@gmail.com'}
+            keyboardType={'email-address'}
+          />
+        </View>
       </ScrollView>
-      <View style={{backgroundColor}}>
-        <TextInput
-          style={{
-            width: '75%',
-            justifyContent: 'center',
-          }}
-          accessibilityStates
-          label="Email"
-          returnKeyType="done"
-          disabled={true}
-          value={'test@gmail.com'}
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={{
-            width: '75%',
-            justifyContent: 'center',
-          }}
-          accessibilityStates
-          label="Phone"
-          disabled={true}
-          returnKeyType="done"
-          value={'123-456-789'}
-        />
-      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   scrollViewContent: {
+    flex: 1,
     paddingTop: 30,
     paddingHorizontal: 30,
-    justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
     marginTop: 20,
     fontWeight: 'normal',
   },
-  centerText: {
-    textAlign: 'center',
+  emailContainer: {
+    display: 'flex',
+    flexDirection: 'row',
   },
-  button: {
-    marginTop: 20,
+  icon: {
+    marginLeft: 25,
+    paddingRight: 10,
+    paddingTop: 25,
   },
 });
