a44eee32ff1d0001dc062061c13262f050e0db83
Mario Herrera <mario.herrera.flores@gmail.com>
2020-05-29 00:04:10 -0700

    Profile component improvements

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,35 +1,78 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-// import {ScrollView, StyleSheet} from 'react-native';
-// import {Headline, Caption, useTheme, Button} from 'react-native-paper';
+import {ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Divider, useTheme, Avatar, Title} from 'react-native-paper';
+import TextInput from '../components/TextInput';
 
-// import overlay from './overlay';
-import BackButton from './BackButton';
-import {Navigation} from '../types';
+import overlay from './overlay';
 
-type Props = {
-  navigation: Navigation;
+// type Props = {
+//   navigation: Navigation;
+// };
+
+export const Profile = () => {
+  const theme = useTheme();
+  const backgroundColor = overlay(2, theme.colors.surface) as string;
+
+  return (
+    <>
+      <ScrollView
+        style={{backgroundColor}}
+        contentContainerStyle={[styles.scrollViewContent, {backgroundColor}]}>
+        <TouchableOpacity style={{marginLeft: 10}} onPress={() => {}}>
+          <Avatar.Image
+            accessibilityStates
+            source={require('../../src/assets/avatar.jpg')}
+            size={150}
+          />
+        </TouchableOpacity>
+        <Title style={styles.title}>Mario Herrera</Title>
+        <Divider accessibilityStates />
+      </ScrollView>
+      <View style={{backgroundColor}}>
+        <TextInput
+          style={{
+            width: '75%',
+            justifyContent: 'center',
+          }}
+          accessibilityStates
+          label="Email"
+          returnKeyType="done"
+          disabled={true}
+          value={'test@gmail.com'}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={{
+            width: '75%',
+            justifyContent: 'center',
+          }}
+          accessibilityStates
+          label="Phone"
+          disabled={true}
+          returnKeyType="done"
+          value={'123-456-789'}
+        />
+      </View>
+    </>
+  );
 };
 
-export const Profile = ({navigation}: Props) => {
-  //   const theme = useTheme();
-
-  //   const backgroundColor = overlay(2, theme.colors.surface) as string;
-
-  return <></>;
-};
-
-// const styles = StyleSheet.create({
-//   scrollViewContent: {
-//     flex: 1,
-//     paddingHorizontal: 30,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   centerText: {
-//     textAlign: 'center',
-//   },
-//   button: {
-//     marginTop: 20,
-//   },
-// });
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    paddingTop: 30,
+    paddingHorizontal: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 20,
+    fontWeight: 'normal',
+  },
+  centerText: {
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: 20,
+  },
+});
